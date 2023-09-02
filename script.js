b94fe691b1bb177beabeac571e721345da3f85e0
abtin <ehwjr0887@naver.com>
2023-09-02 15:00:50 +0900

    complete

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const spreadsheet_container = document.querySelector("#spreadsheet_container");
-
+const export_btn = document.querySelector("#export_btn");
 const rows = 10;
 const columns = 10;
 const spreadsheet = [];
@@ -33,15 +33,45 @@ const alphabets = [
 ];
 
 class Cell {
-  constructor(isHeader, disabled, data, row, column, active = false) {
+  constructor(
+    isHeader,
+    disabled,
+    data,
+    row,
+    column,
+    row_name,
+    column_name,
+    active = false
+  ) {
     this.isHeader = isHeader;
     this.disabled = disabled;
     this.data = data;
     this.row = row;
     this.column = column;
+    this.row_name = row_name;
+    this.column_name = column_name;
     this.active = active;
   }
 }
+
+export_btn.onclick = function (e) {
+  let csv = "";
+  for (let i = 0; i < spreadsheet.length; i++) {
+    csv +=
+      spreadsheet[i]
+        .filter((item) => !item.isHeader)
+        .map((item) => item.data)
+        .join(",") + "\r\n";
+  }
+  const csv_object = new Blob([csv]);
+  const csv_url = URL.createObjectURL(csv_object);
+  console.log(csv, csv_url);
+
+  const a = document.createElement("a");
+  a.href = csv_url;
+  a.download = "Exported Spreadsheet.csv";
+  a.click();
+};
 
 init_spreadsheet();
 
@@ -68,17 +98,30 @@ function init_spreadsheet() {
       if (!cell_data) {
         cell_data = "";
       }
-      const cell = new Cell(isHeader, isDisabled, cell_data, i, j, false);
+
+      const row_name = i;
+      const column_name = alphabets[j - 1];
+      const cell = new Cell(
+        isHeader,
+        isDisabled,
+        cell_data,
+        i,
+        j,
+        row_name,
+        column_name,
+        false
+      );
       spreadsheet_row.push(cell);
     }
     spreadsheet.push(spreadsheet_row);
+    console.log(spreadsheet);
   }
 
-  console.log("spreadsheet", spreadsheet.length, spreadsheet);
   draw_sheet();
 }
 
 function draw_sheet() {
+  spreadsheet_container.innerHTML = "";
   for (let i = 0; i < spreadsheet.length; i++) {
     const rowContainer_element = document.createElement("div");
     rowContainer_element.className = "cell_row";
@@ -93,8 +136,53 @@ function draw_sheet() {
 function create_cellElement(cell) {
   const cell_element = document.createElement("input");
   cell_element.className = "cell";
-  cell_element.id = "cell_" + cell.column + cell.row;
+  cell_element.id = "cell_" + cell.row + cell.column;
   cell_element.value = cell.data;
   cell_element.disabled = cell.disabled;
+
+  if (cell.isHeader) {
+    cell_element.classList.add("header");
+  }
+  cell_element.onclick = () => handle_cellClick(cell);
+  cell_element.onchange = (e) => handle_onChange(e.target.value, cell);
   return cell_element;
+}
+
+function handle_cellClick(cell) {
+  clearHeader_states();
+  const column_header = spreadsheet[0][cell.column];
+  const row_header = spreadsheet[cell.row][0];
+  const column_headerElement = getElement_fromRowColumn(
+    column_header.row,
+    column_header.column
+  );
+  const row_headerElement = getElement_fromRowColumn(
+    row_header.row,
+    row_header.column
+  );
+  column_headerElement.classList.add("active");
+  row_headerElement.classList.add("active");
+
+  document.querySelector("#cell_status").innerHTML =
+    cell.column_name + "" + cell.row_name;
+}
+
+function handle_onChange(data, cell) {
+  cell.data = data;
+}
+
+function clearHeader_states() {
+  for (let i = 0; i < spreadsheet.length; i++) {
+    for (let j = 0; j < spreadsheet[i].length; j++) {
+      const cell = spreadsheet[i][j];
+      if (cell.isHeader) {
+        let cell_element = getElement_fromRowColumn(cell.row, cell.column);
+        cell_element.classList.remove("active");
+      }
+    }
+  }
+}
+
+function getElement_fromRowColumn(row, col) {
+  return document.querySelector("#cell_" + row + col);
 }
